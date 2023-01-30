cfc59f49a1e48d4f6bd7e1f54311633cf2da697b
Diego V <rockandroll.dv@gmail.com>
2023-01-30 18:15:14 -0300

    tercera escritura en el repositorio

--- js/function.js
+++ js/function.js
@@ -1,3 +1,42 @@
+/*
+document.getElementById('boton').onclick = function () {
+    console.log("capturamos el evento click");
+    document.getElementById("demo").innerHTML = "estamos probando nuestro primer evento en js"
+}
 
+document.addEventListener('click',function()
+{
+    console.log("Hola Mundo desde EventListener");
+    document.getElementById("demo").innerHTML = "estamos probando nuestro primer evento en js"
+})
+*/
 
-console.log("Hola Mundo!");
+document.getElementById('boton').addEventListener('click',function()
+{
+    console.log("Hola Mundo desde EventListener");
+    document.getElementById("demo").innerHTML = "estamos probando nuestro primer evento en js"
+    const collection = document.getElementsByClassName("prueba");
+for (let i = 0; i < collection.length; i++) {
+  collection[i].style.backgroundColor = "green";
+}
+})
+
+document.getElementById('Fondo_rojo').addEventListener('click',function()
+{
+    document.body.style.backgroundColor = '#FF0000';    
+})
+
+document.getElementById('Fondo_negro').addEventListener('click',function()
+{
+    document.body.style.backgroundColor = '#000000';    
+})
+
+document.getElementById('Ocultar').addEventListener('click',function()
+{
+    document.getElementById('demo').style.display = 'none';  
+})
+
+/*const collection = document.getElementsByClassName("prueba");
+for (let i = 0; i < collection.length; i++) {
+  collection[i].style.backgroundColor = "green";
+}*/
